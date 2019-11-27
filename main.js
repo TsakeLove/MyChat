da91ed1f847a0f1ec21f1fbe139f9b3e02d5b9b7
Ihor <tsake.love@gmail.com>
2019-11-27 17:06:36 +0200

    Mistakes
    For correct operation and future deployment

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
-const app = require ("express")();
+'use strict';
+const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-let path = require('path');
-let port = 8800;
-let online = [];
+const path = require('path');
+const port = 8800;
+// const online = [];
 // array to store current users
-let users = [];
+const users = [];
 // array for storing current messages
-let messages = [];
+// const messages = [];
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'auth.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'auth.html'));
 });
 
-app.get('/:id', function (req, res) {
+app.get('/:id', (req, res) => {
 
-    if (req.params.id == '') {
-        res.sendFile(path.join(__dirname, 'client.js'));
-    }
-    else if (req.params.id == 'favicon.ico') {
-        res.sendStatus(404);
-    }
-    else {
+  if (req.params.id === '') {
+    res.sendFile(path.join(__dirname, 'client.js'));
+  } else if (req.params.id === 'favicon.ico') {
+    res.sendStatus(404);
+  } else {
 
-        users.push(req.params.id);
-        res.sendFile(path.join(__dirname, 'index.html'));
-    }
+    users.push(req.params.id);
+    res.sendFile(path.join(__dirname, 'index.html'));
+  }
 
-})
+});
 // connection setup
-io.on('connection', function (socket) {
-    connections.push(socket);
-    console.log(users)
-    console.log('Connected: %s sockets connected', connections.length);
-    // end of connection
-    socket.on('disconnect', function (data) {
-        let index = connections.indexOf(socket)
-        // remove a broken connection from the list of current connections
-        let deletedItem = connections.splice(index, 1);
-        // remove user from array of current users
-        users.splice(index, 1);
-        // update the list of users on the client
-        io.sockets.emit('users loaded', { users: users })
-        console.log('Disconnected: %s sockets connected', connections.length);
-    });
+io.on('connection', (socket) => {
+  let connections;
+  connections.push(socket);
+  console.log(users);
+  console.log('Connected: %s sockets connected', connections.length);
+  // end of connection
+  socket.on('disconnect', () => {
+    const index = connections.indexOf(socket);
+    // remove a broken connection from the list of current connections
+    // const deletedItem = connections.splice(index, 1);
+    // remove user from array of current users
+    users.splice(index, 1);
+    // update the list of users on the client
+    io.sockets.emit('users loaded', { users });
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
 
 
 
 });
 
-server.listen(port, function () {
-    console.log('app running on port ' + port);
-})
+server.listen(port, () => {
+  console.log('app running on port ' + port);
+});
